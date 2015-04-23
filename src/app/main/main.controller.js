3a9a1c2c0dc4165debe89351730e0b2bccf71931
John Alexander <jalexander788@gmail.com>
2015-04-23 15:41:38 -0400

    we have rest api data thank god

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,9 +1,13 @@
 'use strict';
 
 angular.module('dailysteals')
-    .controller('MainController', ['api', function (api) {
-        var self = this;
-        api.woot.then(function (data) {
-            self.wootData = data.results;
-        });
-    }]);
+
+.controller('MainCtrl', ['api', function (api) {
+    var self = this;
+
+    api.woot.then(function (data) {
+        self.wootData = data.results;
+        console.log(data.results.collection1[0].price);
+
+    });
+}]);
