a0f22aa72bfebe9868cf87f9c5b1d86c01e895c8
John Alexander <jalexander788@gmail.com>
2015-04-22 22:17:38 -0400

    more stuff that doesnt work

--- src/app/index.js
+++ src/app/index.js
@@ -1,20 +1,22 @@
 'use strict';
 
 angular.module('dailysteals', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'restangular', 'ui.router', 'ui.bootstrap'])
-    .config(function ($stateProvider, $urlRouterProvider) {
-        $stateProvider
-            .state('home', {
-                url: '/',
-                templateUrl: 'app/main/main.html',
-                controller: 'MainCtrl'
-            });
+    .config(['RestangularProvider', function (RestangularProvider, $stateProvider) {
+        RestangularProvider.setBaseUrl('https://www.kimonolabs.com/api/as777lwi');
+        RestangularProvider.setDefaultRequestParams({
+            apikey: 'lxZzI5UKXbWL2JK3DL0U2g2uCfMjFUgd',
+        });
+        console.log(RestangularProvider);
+    }]);
 
-        $urlRouterProvider.otherwise('/');
-    });
-//    .config(['RestangularProvider', function (RestangularProvider, $stateProvider) {
-//        RestangularProvider.setBaseUrl('https://www.kimonolabs.com/api/as777lwi');
-//        RestangularProvider.setDefaultRequestParams({
-//            apikey: 'lxZzI5UKXbWL2JK3DL0U2g2uCfMjFUgd',
-//        });
-//        console.log(RestangularProvider);
-//    }]);
+
+
+//    .config(function ($stateProvider, $urlRouterProvider) {
+//        $stateProvider
+//            .state('home', {
+//                url: '/',
+//                templateUrl: 'app/main/main.html',
+//                controller: 'MainCtrl'
+//            });
+//        $urlRouterProvider.otherwise('/');
+//    });
