aa573d5037a79685eb3a46bf6dd5974e41fa32d9
John Alexander <jalexander788@gmail.com>
2015-06-05 01:25:38 -0400

    cors works on chrome

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -18,23 +18,26 @@ angular.module('dailysteals')
 	if (this.user){
 		this.loginbox = false;
 	} 
-    api.woot.then(function (data) {
+        api.woot.then(function (data) {
         self.wootData = data.results.collection1[0];
     });
-    api.dealgenius.then(function (data) {
+        api.dealgenius.then(function (data) {
         self.dealgeniusData = data.results.collection1[0];        
     });
-      api.wootshirt.then(function (data) {
+        api.wootshirt.then(function (data) {
         self.wootShirtData = data.results.collection1[0]; 
     });
-       api.woothome.then(function (data) {
+        api.woothome.then(function (data) {
         self.wootHomeData = data.results.collection1[0];
     });
-       api.wootsport.then(function (data) {
+        api.wootsport.then(function (data) {
         self.wootSportData = data.results.collection1[0];
     });
        api.wootelectronics.then(function (data) {
         self.wootElectronicsData = data.results.collection1[0];
+    });
+        api.wootcomputers.then(function (data) {
+        self.wootComputersData = data.results.collection1[0];
     });
         api.ebay.then(function (data) {
         self.ebayData = data.results.collection1[0];  
@@ -45,8 +48,8 @@ angular.module('dailysteals')
         api.yugster.then(function (data) {
         self.yugsterData = data.results.collection1[0];  
     });
-        Ebayapi.all('').getList();  
-        
+        Ebayapi.all('').getList(); 
+        console.log(Ebayapi)
     
     var userInfo = new Firebase('https://givemethatthing.firebaseio.com/users');
  
