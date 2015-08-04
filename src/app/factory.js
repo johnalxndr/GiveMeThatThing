e80025288f5b20477963cb5b903b3ad85a865f48
John Alexander <jalexander788@gmail.com>
2015-08-04 11:52:13 -0400

    updating api things

--- src/app/factory.js
+++ src/app/factory.js
@@ -6,14 +6,6 @@ angular.module('dailysteals')
 .factory('api', ['Restangular', function (Restangular) {
     return {
         dealgenius: Restangular.one('af2vnwdy').get(),
-        woot: Restangular.one('as777lwi').get(),
-        wootelectronics: Restangular.one('9ye1jinc').get(),
-        wootshirt: Restangular.one('7uigadhy').get(),
-        wootsport: Restangular.one('8vsr5cbi').get(),
-        woothome: Restangular.one('1wvn1eqw').get(),
-        wootcomputers: Restangular.one('ebxbpc0e').get(),
-        woottoolsgarden: Restangular.one('42u4f4ew').get(),
-        ebay:Restangular.one('6mt8zb5y').get(),
         amazon:Restangular.one('3oned4iq').get(),
         yugster:Restangular.one('d5ww42ru').get()
     };
